f61fb63ff3c6053e3d200093978e773eef4fd728
Ziyan Wang <zw301@nyu.edu>
2018-05-10 16:17:41 -0700

    Add project items

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,7 +57,7 @@
 		document.getElementById("navbar").style.display = "block";
 
 	}
-	document.querySelectorAll('a').forEach(anchor => {
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	    anchor.addEventListener('click', function (e) {
 	        e.preventDefault();
 
